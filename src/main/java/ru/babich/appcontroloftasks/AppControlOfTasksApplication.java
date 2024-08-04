4009cd694ca08858555c00ee957a787b350e87a7
vaddy_b <vadim.krsk.94@gmail.com>
2024-08-04 23:45:58 +0700

    first commit

---
+++ src/main/java/ru/babich/appcontroloftasks/AppControlOfTasksApplication.java
@@ -0,0 +1,13 @@
+package ru.babich.appcontroloftasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppControlOfTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppControlOfTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/babich/appcontroloftasks/AppControlOfTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.babich.appcontroloftasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppControlOfTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
